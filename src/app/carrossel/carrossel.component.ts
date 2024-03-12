a7a05c519a5a34d10b13f670cddb48707966f1bf
hideki <hideki-abe@hotmail.com>
2024-03-12 02:00:33 -0300

    Impl: modelo de carrossel

--- src/app/carrossel/carrossel.component.ts
+++ src/app/carrossel/carrossel.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+
+interface carouselImage {
+  imageSrc: string;
+  imageAlt: string;
+}
 
 @Component({
   selector: 'app-carrossel',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './carrossel.component.html',
   styleUrl: './carrossel.component.css'
 })
-export class CarrosselComponent {
+export class CarrosselComponent implements OnInit{
+
+  @Input() indicators = true;
+  selectedIndex = 0;
+
+  ngOnInit() {
+
+  }
+
+  selectImage(index: number) {
+    this.selectedIndex = index;
+  }
+
+  images:carouselImage[] = [
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1460627390041-532a28402358?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'nature1',
+    },
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'nature2',
+    },
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1640844444545-66e19eb6f549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+      imageAlt: 'person1',
+    },
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'person2',
+    },
+  ]
 
 }
