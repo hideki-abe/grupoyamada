f41ddac667a66d7e9f7eaee5a034da0908565516
hidekiabe <hideki-abe@hotmail.com>
2024-03-12 17:53:29 -0300

    Impl: cards da página Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {CarrosselComponent} from "../carrossel/carrossel.component";
+import {CardComponent} from "../card/card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    CarrosselComponent
+    CarrosselComponent,
+    CardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
