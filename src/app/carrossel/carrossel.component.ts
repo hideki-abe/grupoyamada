3a0cb28e4c713fed814b0bdb4358d620dee3d137
hidekiabe <hideki-abe@hotmail.com>
2024-03-18 10:02:12 -0300

    hotfix: correção de cards da página Home

--- src/app/carrossel/carrossel.component.ts
+++ src/app/carrossel/carrossel.component.ts
@@ -69,7 +69,7 @@ export class CarrosselComponent implements OnInit{
     },
     {
       imageSrc:
-        'https://images.unsplash.com/photo-1640844444545-66e19eb6f549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+        'https://firebasestorage.googleapis.com/v0/b/yamada-fotos.appspot.com/o/CARROSSEL.png?alt=media&token=178f1c50-a7ed-4937-a434-23091d36b749',
       imageAlt: 'person1',
     },
     {
