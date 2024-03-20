bea2861037b80c9276d0cfef757dbc88da451005
hidekiabe <hideki-abe@hotmail.com>
2024-03-20 17:52:27 -0300

    Impl: componente de produtos, cards e correção no css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import {SobreNosComponent} from "./pages/sobre-nos/sobre-nos.component";
+import {ProdutosComponent} from "./pages/produtos/produtos.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-  { path: 'sobre', component: SobreNosComponent}
+  { path: 'sobre', component: SobreNosComponent},
+  { path: 'produtos', component: ProdutosComponent}
 ];
--- src/app/pages/produtos/produtos.component.ts
+++ src/app/pages/produtos/produtos.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {ProdutoComponent} from "../../components/produto/produto.component";
+import {CardComponent} from "../../components/card/card.component";
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [],
+  imports: [
+    ProdutoComponent,
+    CardComponent
+  ],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
 })
