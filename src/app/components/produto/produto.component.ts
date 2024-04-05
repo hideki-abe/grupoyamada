7921d6151e754545d30d28e48f11057162ae23ab
hidekiabe <hideki-abe@hotmail.com>
2024-04-05 17:15:40 -0300

    Impl: imagens dos produtos

--- src/app/components/produto/produto.component.ts
+++ src/app/components/produto/produto.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Produto} from "../../interfaces/produto";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-produto',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './produto.component.html',
   styleUrl: './produto.component.css'
 })
