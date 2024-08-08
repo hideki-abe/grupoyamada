1f6abfd693238bfef46642a734074caa986f57bb
hidekiabe <hideki-abe@hotmail.com>
2024-08-08 16:54:02 -0300

    Impl: paginas de serviços e orçamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import {SobreNosComponent} from "./pages/sobre-nos/sobre-nos.component";
 import {ProdutosComponent} from "./pages/produtos/produtos.component";
+import {ServicosComponent} from "./pages/servicos/servicos.component";
+import {OrcamentosComponent} from "./pages/orcamentos/orcamentos.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'sobre', component: SobreNosComponent},
-  { path: 'produtos', component: ProdutosComponent}
+  { path: 'produtos', component: ProdutosComponent},
+  { path: 'servicos', component: ServicosComponent},
+  { path: 'orcamentos', component: OrcamentosComponent}
 ];
