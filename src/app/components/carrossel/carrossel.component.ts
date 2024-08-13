2117746dc7ff3ddc368f8afd90953dd812033949
hideki <hideki-abe@hotmail.com>
2024-08-13 03:07:00 -0300

    Hotfix: correção das páginas

--- src/app/components/carrossel/carrossel.component.ts
+++ src/app/components/carrossel/carrossel.component.ts
@@ -61,22 +61,7 @@ export class CarrosselComponent implements OnInit{
       imageSrc:
         '/assets/carrossel1.png',
       imageAlt: 'nature1',
-    },
-    {
-      imageSrc:
-        'https://firebasestorage.googleapis.com/v0/b/yamada-fotos.appspot.com/o/WhatsApp%20Image%202024-02-29%20at%2016.36.27.jpeg?alt=media&token=f88a7e42-e207-46eb-b51e-987478ba9421',
-      imageAlt: 'nature2',
-    },
-    {
-      imageSrc:
-        'https://firebasestorage.googleapis.com/v0/b/yamada-fotos.appspot.com/o/CARROSSEL.png?alt=media&token=178f1c50-a7ed-4937-a434-23091d36b749',
-      imageAlt: 'person1',
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      imageAlt: 'person2',
-    },
+    }
   ]
 
 }
--- src/app/pages/servicos/servicos.component.ts
+++ src/app/pages/servicos/servicos.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {CardComponent} from "../../components/card/card.component";
 
 @Component({
   selector: 'app-servicos',
   standalone: true,
-  imports: [],
+    imports: [
+        CardComponent
+    ],
   templateUrl: './servicos.component.html',
   styleUrl: './servicos.component.css'
 })
