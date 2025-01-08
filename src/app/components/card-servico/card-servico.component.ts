10d642b837bfd15cb5640c8aa4d9df1b2f58920b
hidekiabe <hideki-abe@hotmail.com>
2025-01-08 16:32:51 -0300

    Impl: nossos servicos

---
+++ src/app/components/card-servico/card-servico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-servico',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-servico.component.html',
+  styleUrl: './card-servico.component.css'
+})
+export class CardServicoComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import {CarrosselComponent} from "../../components/carrossel/carrossel.component
 import {CardQualidadeComponent} from "../../components/card-qualidade/card-qualidade.component";
 import {PreviaProdutosComponent} from "../../components/previa-produtos/previa-produtos.component";
 import {QuemSomosComponent} from "../../components/quem-somos/quem-somos.component";
+import {CardServicoComponent} from "../../components/card-servico/card-servico.component";
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,8 @@ import {QuemSomosComponent} from "../../components/quem-somos/quem-somos.compone
     CarrosselComponent,
     CardQualidadeComponent,
     PreviaProdutosComponent,
-    QuemSomosComponent
+    QuemSomosComponent,
+    CardServicoComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
