bd588b2ce10c9b201d27310f7e3c98d4bb24337e
hidekiabe <hideki-abe@hotmail.com>
2025-01-21 08:52:43 -0300

    Att

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,25 +1,40 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {NgOptimizedImage, NgStyle} from "@angular/common";
 import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [
     NgOptimizedImage,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NgStyle
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  visibility = 'visible';
+  color = 'red';
+  background = 'red';
+  transform = 'translateX(0)';
+
   constructor(private router: Router) {
   }
 
   onClick() {
     this.router.navigate(['/sobre']);
+  }
+
+  onClickMenu() {
+    if( this.transform == 'translateX(-100%)') {
+      this.transform = 'translate(100%)';
+    } else {
+      this.transform == 'translateX(-100%)'
+    }
   }
 
 }
