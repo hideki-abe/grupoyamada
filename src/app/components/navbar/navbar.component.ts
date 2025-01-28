34123d2a5dd3e4706bcb80a3e7f8570191061ecf
hidekiabe <hideki-abe@hotmail.com>
2025-01-28 17:05:05 -0300

    Hotfix: correcao de menu responsivo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, HostListener} from '@angular/core';
 import {NgOptimizedImage, NgStyle} from "@angular/common";
 import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 
@@ -17,20 +17,27 @@ import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 })
 export class NavbarComponent {
 
-  visibility = 'visible';
   color = 'red';
   background = 'red';
   transform = '';
+  innerWidth = window.innerWidth;
 
   constructor(private router: Router) {
   }
 
-  onClick() {
-    this.router.navigate(['/sobre']);
+  @HostListener('window:resize', ['$event'])
+  widthListener() {
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth >= 800 || this.transform == 'translate(-100%)') {
+      this.transform = '';
+    }
+    console.log(this.transform);
   }
 
   onClickMenu() {
-    if( this.transform == 'translateX(-100%)') {
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth);
+    if( this.transform == 'translateX(-100%)' || this.transform == '') {
       this.transform = 'translate(0)';
     } else {
       this.transform = 'translateX(-100%)'
